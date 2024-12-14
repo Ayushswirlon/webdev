013011f3c34cc7c5c3c3954d5067ae95da651213
Ayushswirlon <ayushcarpenter7777@gmail.com>
2024-12-14 13:54:46 +0530

    added a logger to our proj

--- backend/02_express/index.js
+++ backend/02_express/index.js
@@ -1,10 +1,31 @@
+import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let teaData = [];
 let nextId = 1;
@@ -14,6 +35,7 @@ app.post("/teas", (req, res) => {
   const { name, price } = req.body;
   let teaName = { id: nextId++, name, price };
   teaData.push(teaName);
+  logger.info("tea created");
   res.status(201).send(teaName);
 });
 
